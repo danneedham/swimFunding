79de8e99d9bafd4d6785a82152cfd0c14f2ee5b8
Dan Needham <dcn3@williams.edu>
2018-04-23 19:15:23 -0400

    work

--- SwimFundData.py
+++ SwimFundData.py
@@ -7,7 +7,24 @@ def readDB(database):
         csvf = csv.reader(f)
         f.readline()
         for line in csvf:
-            result.append(line)
+            result.append(parseSchool(line))
     return result
 
-print(readDB("SwimFundingData.csv"))
+
+
+def parseSchool(l):
+    """Takes the list printed out for each school and returns a list with the variables we care about.
+    """
+    school = l[0]
+    division = l[3][:4] + ' ' + l[3][6:8]
+    scholarship = l[6]
+    for i in range(len(scholarship)-1):
+        #if scholarship[i] == "\xca":
+        #    scholarship = scholarship[:i+1]
+        print(scholarship[i])
+
+    return [school, division, scholarship]
+
+test = readDB("SwimFundingData.csv")
+print(parseSchool(test))
+    
